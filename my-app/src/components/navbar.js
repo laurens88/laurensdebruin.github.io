09fb42ce6b5daa74cedc4396fc08759e0f951dab
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-07-05 21:16:26 +0200

    Install roboto mono font

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -9,12 +9,17 @@ function Navbar() {
 
     return (
         <div className={styles.navbar}>
+            <Stack direction="row" spacing={10}>
             <Stack direction="row" spacing={{ xs: 0, sm: 1, md: 6 }}>
                 <Button onClick={() => navigate('/')}>Home</Button>
                 <Button onClick={() => navigate('/resume')}>Resume</Button>
                 <Button onClick={() => navigate('/projects')}>Projects</Button>
                 <Button onClick={() => navigate('/photography')}>Photography</Button>
                 <Button onClick={() => navigate('/hobby')}>Hobby</Button>
+            </Stack>
+            <Stack direction="row" spacing={{ xs: 0, sm: 1, md: 6 }}>
+                
+            </Stack>
             </Stack>
         </div>
     );
