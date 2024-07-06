1569114dad3d503e27516c9700285fbf0859525d
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-07-06 14:40:46 +0200

    upload portrait + first version of homepage

--- my-app/src/screens/homePage.js
+++ my-app/src/screens/homePage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/navbar';
 import styles from '../styles/homePage.module.css';
+import Grid from "@mui/material/Grid";
+import Portrait from '../assets/images/portrait.jpg';
 
 function Home() {
     let navigate = useNavigate();
@@ -10,8 +12,23 @@ function Home() {
         <div>
             <Navbar />
             <div className={styles.summary}>
+                <Grid container spacing={2} direction='row'>
+                <Grid item xs={0} md={2}>
+                </Grid>
+                <Grid item xs={6} md={6}>
                 <h1>Laurens de Bruin</h1>
                 <h2>Software Developer</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+                    Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.
+                    Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                </p>
+                </Grid>
+                
+                <Grid item xs={12} md={4}>
+                    <img src={Portrait} alt="Portrait" className={styles.portrait} />
+                </Grid>
+                </Grid>
             </div>
         </div>
     );
