ad340104c1d061099d1a32b9d4acee38233471db
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-07-15 17:20:58 +0200

    add animation to homepage header

--- my-app/src/screens/homePage.js
+++ my-app/src/screens/homePage.js
@@ -23,7 +23,7 @@ function Home() {
         <Grid container spacing={2} direction="row">
           <Grid item xs={2} md={2}></Grid>
           <Grid item xs={7} md={6}>
-            <h1>Laurens de Bruin</h1>
+            <h1 className={styles.blinkingCursor}>Laurens de Bruin</h1>
             <h2>Software Developer</h2>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
