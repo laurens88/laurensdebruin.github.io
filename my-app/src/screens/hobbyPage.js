291a0121f1fbb6620b4aa52071c1bf77c53e13ba
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-07-25 20:17:43 +0200

    add final travel map

--- my-app/src/screens/hobbyPage.js
+++ my-app/src/screens/hobbyPage.js
@@ -2,7 +2,10 @@ import React from "react";
 import Navbar from "../components/navbar";
 import NavDrawer from "../components/drawer";
 import "../App.css";
-import WorldMap from "../components/worldmap";
+import MapEmbed from "../components/worldmap";
+import Map from "../assets/images/worldmap-svg.svg";
+
+//https://www.amcharts.com/visited_countries/index.php#AT,BE,CY,DK,FR,DE,GR,IT,LU,NL,NO,PT,ES,SE,CH,GB,CA,US,QA,TH
 
 function Hobby() {
   return (
@@ -14,7 +17,8 @@ function Hobby() {
         <NavDrawer activeItems={["Hobbies"]} />
       </div>
       <p style={{textAlign: "center"}}>My hobbies here.</p>
-      <WorldMap />
+      {/* <MapEmbed /> */}
+      <img src={Map} alt="map" style={{width: "100%", height: "auto"}} />
     </div>
   );
 }
