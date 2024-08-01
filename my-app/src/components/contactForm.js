acf53aed828c9752d9e7e547a35a064e6a332a62
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-08-01 14:39:07 +0200

    clean code + apply formatting

--- my-app/src/components/contactForm.js
+++ my-app/src/components/contactForm.js
@@ -3,43 +3,43 @@ import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Typography from "@mui/material/Typography";
-import EmailIcon from '@mui/icons-material/Email';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from "@mui/icons-material/Email";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const modalStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '80%',
-  maxWidth: '400px',
-  background: 'linear-gradient(135deg, #f5f7fa, #c3cfe2)',
-  boxShadow: '0 4px 20px rgba(0, 0, 0, 0.1)',
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "80%",
+  maxWidth: "400px",
+  background: "linear-gradient(135deg, #f5f7fa, #c3cfe2)",
+  boxShadow: "0 4px 20px rgba(0, 0, 0, 0.1)",
   p: 4,
-  borderRadius: '12px',
+  borderRadius: "12px",
 };
 
 const buttonStyle = {
   mb: 2,
-  backgroundColor: '#1011F5',
-  color: 'white',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-start', // Align items to the left
-  width: '100%', // Set to full width relative to its container
-  maxWidth: '250px', // Cap the maximum width
-  paddingLeft: '16px', // Add padding to center the content
-  '&:hover': {
-    backgroundColor: '#0d0fb8',
+  backgroundColor: "#1011F5",
+  color: "white",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-start",
+  width: "100%",
+  maxWidth: "250px",
+  paddingLeft: "16px",
+  "&:hover": {
+    backgroundColor: "#0d0fb8",
   },
 };
 
 const iconStyle = {
-  minWidth: '24px', // Ensure consistent icon width
-  marginRight: '8px',
+  minWidth: "24px",
+  marginRight: "8px",
 };
 
 const ContactForm = ({ open, handleClose }) => {
@@ -63,7 +63,7 @@ const ContactForm = ({ open, handleClose }) => {
             component="h2"
             mb={2}
             align="center"
-            sx={{ color: '#1011F5', fontWeight: 'bold' }}
+            sx={{ color: "#1011F5", fontWeight: "bold" }}
           >
             Get in Touch:
           </Typography>
--- my-app/src/screens/homePage.js
+++ my-app/src/screens/homePage.js
@@ -1,12 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Navbar from "../components/navbar";
 import NavDrawer from "../components/drawer";
 import styles from "../styles/homePage.module.css";
 import Grid from "@mui/material/Grid";
 import Portrait from "../assets/images/portrait.jpg";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import FileDownloadButton from "../components/downloadbutton";
-import Send from '@mui/icons-material/Send';
+import Send from "@mui/icons-material/Send";
 import Resume_english from "../assets/downloads/Resume_LaurensdeBruin.pdf";
 import ContactForm from "../components/contactForm";
 import "../App.css";
@@ -32,17 +32,28 @@ function Home() {
             <h1 className={styles.blinkingCursor}>Laurens de Bruin</h1>
             <h2>Software Developer</h2>
             <p>
-            Welcome to my personal website. Here, you will find an overview of my professional experience, 
-            including an extensive version of my resume and various coding projects I’ve worked on, 
-            reflecting my background in AI and my passion for software development. Additionally, 
-            I have included a page dedicated to showcasing my photography hobby and another focused on my travels. 
-            Feel free to explore and learn more about my work and interests.
+              Welcome to my personal website. Here, you will find an overview of
+              my professional experience, including an extensive version of my
+              resume and various coding projects I’ve worked on, reflecting my
+              background in AI and my passion for software development.
+              Additionally, I have included a page dedicated to showcasing my
+              photography hobby and another focused on my travels. Feel free to
+              explore and learn more about my work and interests.
             </p>
-            <Button variant="contained" endIcon={<Send/>} style={{margin: '10px', backgroundColor: '#1011F5'}} onClick={handleContactOpen}>
+            <Button
+              variant="contained"
+              endIcon={<Send />}
+              style={{ margin: "10px", backgroundColor: "#1011F5" }}
+              onClick={handleContactOpen}
+            >
               Contact
             </Button>
 
-            <FileDownloadButton text="Resume" file={Resume_english} filename="Resume_LaurensdeBruin.pdf" />
+            <FileDownloadButton
+              text="Resume"
+              file={Resume_english}
+              filename="Resume_LaurensdeBruin.pdf"
+            />
           </Grid>
 
           <Grid item xs={12} md={4}>
