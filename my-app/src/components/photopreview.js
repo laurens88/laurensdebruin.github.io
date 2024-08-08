8a76a9a4f49c1ccdae0ccc0b1b353e0154e22c4b
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-08-08 19:24:43 +0200

    create photo preview component

---
+++ my-app/src/components/photopreview.js
@@ -0,0 +1,17 @@
+import styles from "../styles/photopreview.module.css";
+
+function PhotoPreview({ photolist }) {
+    let randomPhotos = photolist.sort(() => Math.random() - 0.5).slice(0, 4);
+    return (
+        <div>
+        <h1>Photo Preview</h1>
+        <div className={styles.preview}>
+            {randomPhotos.map((photo, index) => (
+                <img key={index} src={photo} alt="preview" />
+            ))}
+        </div>
+        </div>
+    );
+}
+
+export default PhotoPreview;
--- my-app/src/screens/photographyPage.js
+++ my-app/src/screens/photographyPage.js
@@ -1,21 +1,25 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import NavDrawer from "../components/drawer";
+import { useNavigate } from "react-router-dom";
 import "../App.css";
 import styles from "../styles/photographyPage.module.css";
+import PhotoPreview from "../components/photopreview";
 
 function Photography() {
+  let navigate = useNavigate();
+
   return (
     <div>
       <div className="desktop-nav">
         <Navbar />
+        <PhotoPreview photolist={["/photography/still_life/beach.jpg", "/photography/still_life/plane.jpg", "/photography/still_life/sky.jpg", "/photography/travel/thai_guard.jpg"]} />
       </div>
       <div className="mobile-nav">
         <NavDrawer activeItems={["Photography"]} />
       </div>
       <div className={styles.page}>
       </div>
-
     </div>
   );
 }
