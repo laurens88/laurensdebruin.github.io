de723348ca968e79f518d1cc1b89ac3ae597533b
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-08-09 20:38:05 +0200

    change photos to lowRes in photopreview component to improve loading speed

--- my-app/src/components/photopreview.js
+++ my-app/src/components/photopreview.js
@@ -12,7 +12,7 @@ function PhotoPreview({ photolist, category, title }) {
         <h1>{title}</h1>
         <div className={styles.preview}>
             {randomPhotos.map((photo, index) => (
-                path = '/photography/' + category + '/' + photo,
+                path = '/photography/' + category + '/' + photo.slice(0, -4)+'_low'+photo.slice(-4),
                 <img key={index} src={path} alt="preview" />
             ))}
         </div>
