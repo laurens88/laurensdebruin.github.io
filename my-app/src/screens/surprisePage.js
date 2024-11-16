01f9dda8703aec5dd259a6061c2cafa0e839b783
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-11-16 15:56:39 +0100

    add questions to surprise page

--- my-app/src/screens/surprisePage.js
+++ my-app/src/screens/surprisePage.js
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './surprisePage.module.css';
 import Button from "@mui/material/Button";
 import { styled } from '@mui/material/styles';
+import steganography from "../assets/images/steganography.png";
+import code from "../assets/images/code.png";
 
 function Surprise() {
     const [currentScreen, setCurrentScreen] = useState(0);
@@ -13,6 +15,29 @@ function Surprise() {
             backgroundColor: 'black',
         },
     });
+
+    const ObfuscatedText = ({ text = '', interval = 100 }) => {
+        const [displayedText, setDisplayedText] = useState(text);
+      
+        useEffect(() => {
+          if (!text) return; // Handle cases where text might be undefined or empty
+          
+          const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+          
+          const obfuscate = () => {
+            const obfuscated = text.split('').map((char) => {
+              return char === ' ' ? ' ' : characters[Math.floor(Math.random() * characters.length)];
+            });
+            setDisplayedText(obfuscated.join(''));
+          };
+      
+          const intervalId = setInterval(obfuscate, interval);
+      
+          return () => clearInterval(intervalId); // Clean up interval
+        }, [text, interval]);
+      
+        return <span style={{color: "red"}}>{displayedText}</span>;
+      };
 
     const ScreenOne = ({ onSubmit }) => (
         <div>
@@ -40,31 +65,30 @@ function Surprise() {
             </form>
         </div>
     );
-    
 
-    // const ScreenThree = ({ onBlueClick }) => (
-    //     <div>
-    //         <h3>Q2: Choose your path:</h3>
-    //         <Button
-    //             variant="contained"
-    //             style={{ backgroundColor: 'blue', color: 'white', marginRight: '10px' }}
-    //             onClick={onBlueClick}
-    //         >
-    //             Blue Button
-    //         </Button>
-    //         <Button
-    //             variant="contained"
-    //             style={{ backgroundColor: 'red', color: 'white' }}
-    //             onClick={() => setCurrentScreen(0)}
-    //         >
-    //             Red Button
-    //         </Button>
-    //     </div>
-    // );
+    const Screen3point5 = ({ onSubmit }) => (
+        <div>
+            <h2>4195</h2>
+            <h3>Q3: </h3>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="" />
+            </form>
+        </div>
+    );
+
+    const Screen3point6 = ({ onSubmit }) => (
+        <div>
+            <h3>Q4: What do people do at Loonsche Duynen?</h3>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="" />
+            </form>
+        </div>
+    );
 
     const ScreenFour = ({ onSubmit }) => (
         <div>
-            <h3>Q3: What is the capital of France?</h3>
+            <img src={steganography} style={{width: 400}} alt="steganography" />
+            <h3>Q5: </h3>
             <form onSubmit={onSubmit}>
                 <input type="text" placeholder="" />
             </form>
@@ -73,22 +97,73 @@ function Surprise() {
     
     const ScreenFive = ({ onSubmit }) => (
         <div>
-            <h3>Q4: What is the capital of France?</h3>
+            <h3>Q6: Where does BMW MTC meet? ____ HP</h3>
             <form onSubmit={onSubmit}>
                 <input type="text" placeholder="" />
             </form>
         </div>
     );
 
-    const answers = ["-", "42", "seven", "b"]; // Correct answers for screens
+    const ScreenSix = ({ onSubmit }) => (
+        <div>
+            <h3>Q7: +BIAB + -BIAB</h3>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="" />
+            </form>
+        </div>
+    );
+
+    const ScreenSeven = ({ onSubmit }) => (
+        <div>
+            <h3>Q8: 🟠🔵🏍🖥  1:33:50 ⏸</h3>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="" />
+            </form>
+        </div>
+    );
+
+    const ScreenEight = ({ onSubmit }) => (
+        <div>
+            <img src={code} style={{width: 400}} alt="code" />
+            <h3>Q9: What is printed by this code?</h3>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="" />
+            </form>
+        </div>
+    );
+
+    const ScreenTen = ({ onSubmit }) => (
+        <div>
+            <h3>Q10: </h3>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="" />
+            </form>
+        </div>
+    );
+
+    const ScreenEleven = ({ onSubmit }) => (
+        <div>
+            <h1>🎉 Congratulations! 🎉</h1>
+            <p>You have solved the mystery!</p>
+            <p>Now you can find your reward at ...</p>
+        </div>
+    );
+
+    const answers = ["-", "42", "seven", "4195", "golf", "echo", "5275", "55", "609", "17", "placeholder"];
 
     const screens = [
         <ScreenOne key="screen1" onSubmit={(e) => handleAnswer(e, 0)} />,
         <ScreenTwo key="screen2" onSubmit={(e) => handleAnswer(e, 1)} />,
         <ScreenThree key="screen3" onSubmit={(e) => handleAnswer(e, 2)} />,
-        <ScreenFour key="screen4" onSubmit={(e) => handleAnswer(e, 3)} />,
-        <ScreenFive key="screen5" onSubmit={(e) => handleAnswer(e, 4)} />,
-
+        <Screen3point5 key="screen3.5" onSubmit={(e) => handleAnswer(e, 3)} />,
+        <Screen3point6 key="screen3.6" onSubmit={(e) => handleAnswer(e, 4)} />,
+        <ScreenFour key="screen4" onSubmit={(e) => handleAnswer(e, 5)} />,
+        <ScreenFive key="screen5" onSubmit={(e) => handleAnswer(e, 6)} />,
+        <ScreenSix key="screen6" onSubmit={(e) => handleAnswer(e, 7)} />,
+        <ScreenSeven key="screen7" onSubmit={(e) => handleAnswer(e, 8)} />,
+        <ScreenEight key="screen8" onSubmit={(e) => handleAnswer(e, 9)} />,
+        <ScreenTen key="screen10" onSubmit={(e) => handleAnswer(e, 10)} />,
+        <ScreenEleven key="screen11" />
     ];
 
     const handleAnswer = (e, screenIndex) => {
@@ -111,11 +186,12 @@ function Surprise() {
             {currentScreen === 0 ? (
                 <>
                     <h1>Welcome</h1>
-                    <p>You have been selected to solve this mystery...<br></br>
-                    For each question you can ask a single person for help.
+                    <p>You have been selected by <ObfuscatedText text="Laurens"/> to solve this mystery...<br></br>
+                    For each question you can ask a single person for help (except <ObfuscatedText text="Laurens"/>).<br></br>
+                    Others can google the answers, but you can't. Good luck!
                     </p>
                     <CustomButton onClick={() => setCurrentScreen(1)}>
-                        Collaboratio maximi momenti
+                        I'm ready
                     </CustomButton>
                 </>
             ) : (
